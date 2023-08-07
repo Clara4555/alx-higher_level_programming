61ce6bd2e73d8909bd1562db60548deb0e82a76b
Clara4555 <favourloyalty8@gmail.com>
2023-08-07 07:32:47 -0700

    task 0 & 1

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,24 +1,25 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * struct listint_s - linked list
- * @a: integer
- * @next: points to node
- * Description: node structure for python project
+ *@a: integer
+ *@next: next node
+ *
+ *Description: linked list node structure
  */
 typedef struct listint_s
 {
 	int a;
-	struct listint_s *next;
+	struct listint_s*next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int a);
+size_tprint_listint(const listint_t *h);
 int check_cycle(listint_t *list);
-listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
-
 #endif
+
+
