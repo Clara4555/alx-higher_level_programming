59000e690d6f13823e982d479dc16b6631e25e84
Clara4555 <favourloyalty8@gmail.com>
2023-08-08 17:07:13 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ascii_char in range(ord('a'), ord('z') + 1):    
-    if ascii_char != ord('q') and ascii_char != ord('e'):   
+for ascii_char in range(ord('a'), ord('z') + 1):
+    if ascii_char != ord('q') and ascii_char != ord('e'):
         print("{}".format(chr(ascii_char)), end="")
