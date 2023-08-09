3ccdcddbbef605ff4f55223becf008cbfc21ea49
Clara4555 <favourloyalty8@gmail.com>
2023-08-08 17:39:11 -0700

    5-print_comb2.py

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for print_number in range(0, 100):
+    if print_number == 99:
+        print("{}".format(print_number))
+    else:
+        print("{:02d}".format(print_number), end=" , ")
