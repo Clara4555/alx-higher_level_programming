a28e58bfe4db1b041af5490c72f8c01ba7ae20cd
Clara4555 <favourloyalty8@gmail.com>
2023-08-28 14:51:36 -0700

    3-safe_print_division.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     list_res = 0
     for a in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{}".format(my_list[a]), end="")
             list_res += 1
         except IndexError:
             break
