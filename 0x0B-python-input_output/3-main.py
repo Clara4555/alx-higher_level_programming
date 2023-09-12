129c27d4b126c928c87ced993c2d7d945b0cd22b
Clara4555 <favourloyalty8@gmail.com>
2023-09-12 13:54:00 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/3-main.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+to_json_string = __import__('3-to_json_string').to_json_string
+
+my_list = [1, 2, 3]
+s_my_list = to_json_string(my_list)
+print(s_my_list)
+print(type(s_my_list))
+
+my_dict = { 
+    'id': 12,
+    'name': "John",
+    'places': [ "San Francisco", "Tokyo" ],
+    'is_active': True,
+    'info': {
+        'age': 36,
+        'average': 3.14
+    }
+}
+s_my_dict = to_json_string(my_dict)
+print(s_my_dict)
+print(type(s_my_dict))
+
+try:
+    my_set = { 132, 3 }
+    s_my_set = to_json_string(my_set)
+    print(s_my_set)
+    print(type(s_my_set))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+guillaume@ubuntu:~/0x0B$ ./3-main.py
+[1, 2, 3]
+<class 'str'>
+{"id": 12, "is_active": true, "name": "John", "info": {"average": 3.14, "age": 36}, "places": ["San Francisco", "Tokyo"]}
+<class 'str'>
+[TypeError] {3, 132} is not JSON serializable
+guillaume@ubuntu:~/0x0B$ 
+No test cases needed
+
+Repo:
+
+GitHub repository: alx-higher_level_programming
+Directory: 0x0B-python-input_output
+File: 3-to_json_string.py
+   
+4. From JSON string to Object
+mandatory
+Write a function that returns an object (Python data structure) represented by a JSON string:
+
+Prototype: def from_json_string(my_str):
+You don’t need to manage exceptions if the JSON string doesn’t represent an object.
+guillaume@ubuntu:~/0x0B$ cat 4-main.py
+#!/usr/bin/python3
+from_json_string = __import__('4-from_json_string').from_json_string
+
+s_my_list = "[1, 2, 3]"
+my_list = from_json_string(s_my_list)
+print(my_list)
+print(type(my_list))
+
+s_my_dict = """
+{"is_active": true, "info": {"age": 36, "average": 3.14}, 
+"id": 12, "name": "John", "places": ["San Francisco", "Tokyo"]}
+"""
+my_dict = from_json_string(s_my_dict)
+print(my_dict)
+print(type(my_dict))
+
+try:
+    s_my_dict = """
+    {"is_active": true, 12 }
+    """
+    my_dict = from_json_string(s_my_dict)
+    print(my_dict)
+    print(type(my_dict))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
