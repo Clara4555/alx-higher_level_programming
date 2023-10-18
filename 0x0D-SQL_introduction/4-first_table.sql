288b0612cb62d5ccc2d1895ad6ebb2fad10f55f0
Clara4555 <favourloyalty8@gmail.com>
2023-10-18 14:04:39 -0700

    task4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- script that creates a table called first_table in the current database in your MySQL server
+-- first_table description:
+-- name VARCHAR(256) , id INT
+CREATE TABLE IF NOT EXISTS `first_table`(`id` INT, `name` VACHAR(256));
